b66d382f774004c9de2fb9776002f043ae62cf3c
seo1224 <43231985+seo1224@users.noreply.github.com>
2018-12-06 15:57:45 +0900

    code change 4

--- linkedList.c
+++ linkedList.c
@@ -12,16 +12,39 @@ static linknd_t *list; // linked list 실체
 linknd_t* create_node(int value){
 	linknd_t* ndPtr;
 	
-	ndPtr = (linknd_t*)malloc( sizeof(linknd_t) ); // 동적 메모리 할당
+	// 동적 메모리 할당
+	ndPtr = (linknd_t*)malloc( sizeof(linknd_t) );
+	
 	if(ndPtr == NULL)
 	{
 		printf("ERROR\n");
 		return NULL;
 	}
 	
-    ndPtr->data = value; //정수값 저장
+	//정수값 저장
+    ndPtr->data = value; 
 	ndPtr->next = NULL;
 	
-	return ndPtr; 
+	return ndPtr; 	
+}
+
+void addtail(int value){
+	linknd_t *ndPtr, *newPtr;
+	
+	if(list == NULL)
+	{
+		return;
+	}
+	else 
+	{
+		ndPtr = list;
+		while (ndPtr->next != NULL){
+			ndPtr = ndPtr->next;
+		}
+		
+		newPtr = create_node(value);
+		ndPtr->next = newPtr;
+	}
 	
 }
+
